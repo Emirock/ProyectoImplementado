1338a0691a4039f4489c1439e9dc8ed2ce7096d9
Emiliano Pineda <emi.rock11@hotmail.com>
2018-10-26 15:11:07 -0500

    Se añadió el script para el ataque especial

---
+++ Assets/Scripts/Tesla/TeslaBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeslaBall : MonoBehaviour {
+
+    public int IsRight = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Rotate(1.0f, 1.0f,1.0f);
+        transform.Translate(Time.deltaTime*5.0f*IsRight,0,0);
+	}
+
+    void ChangeSide()
+    {
+        IsRight = -1;
+    }
+
+    
+
+}
